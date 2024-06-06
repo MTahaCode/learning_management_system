4e4c8bdd0405b07859d4008c204bc679f5d46bb3
AffanWajid12 <affanwajidali@gmail.com>
2024-06-06 16:38:56 +0500

    fixed assignment

--- lms_backend/lms_backend/src/main/java/com/backend/lms_backend/entity/assignment.java
+++ lms_backend/lms_backend/src/main/java/com/backend/lms_backend/entity/assignment.java
@@ -1,10 +1,12 @@
 package com.backend.lms_backend.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class assignment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
